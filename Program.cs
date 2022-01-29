63783fce9be1da90461224cacb8b00f084b33956
VishalPalode <96374249+VishalPalode@users.noreply.github.com>
2022-01-30 01:08:08 +0530

    CompanyAddedInEmpWage

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWage
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("welcome");
+
+            EmpWageForMultipleComp employee = new EmpWageForMultipleComp();
+            employee.computeEmpwage("DMart", 20, 2, 10);
+            employee.computeEmpwage("Reliance", 10, 4, 20);
+            Console.ReadLine();
+          
+        }
+    }
+}
