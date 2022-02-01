6596c8a6dba69d30f2c519a01388cccc1003e0bc
VishalPalode <96374249+VishalPalode@users.noreply.github.com>
2022-02-01 16:42:03 +0530

    Interface Approach

---
+++ InterfaceEmgWage.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWage
+{
+    internal class InterfaceEmgWage
+    {
+        //Interface for the Employee Wage problem
+        public interface IEmpWageCompute
+        {
+            void AddCompany(string company, int wagePrHrs, int totalWorkHrs, int totalWorkDay);
+            void GetWage();
+        }
+    }
+}
