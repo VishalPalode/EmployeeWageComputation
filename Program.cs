ed1e54a0b2487d9da02294b3ad9ed89af68c8ce8
VishalPalode <96374249+VishalPalode@users.noreply.github.com>
2022-02-01 17:13:48 +0530

    Total wage when queried by company

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWage
+{
+    internal class Program
+    {
+       static void Main(string[] args)
+        {
+            //Creating Object for each company and passing value to constructor.
+            CalculateEmpWage company = new CalculateEmpWage();
+            company.AddCompany("Dmart", 30, 120, 25);
+            company.AddCompany("Reliance", 25, 125, 24);
+            company.AddCompany("Amazon", 40, 110, 22);
+            company.GetWage();
+            Console.WriteLine("Total Wage for Company is: " + company.GetTotalWage("Dmart"));
+            Console.ReadLine();
+        }
+    }
+    
+}
---
+++ SwitchCase.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWage
+{
+    internal class SwitchCase
+    {
+        public static void PerformSwitchUse()
+        {
+            int emphrs;
+            int empWagePrHrs = 20;
+            const int FULL_TIME = 1;           //Constant variables.
+            const int PART_TIME = 2;
+            Random random = new Random();
+            int randomNum = random.Next(0, 3);     //generating random number from 0 to 2.
+            switch (randomNum)                    //passing random number into switch to get employee work hours.
+            {
+                case FULL_TIME:              //passing constant variable in case value.
+                    emphrs = 8;
+                    break;
+                case PART_TIME:
+                    emphrs = 4;
+                    break;
+                default:
+                    emphrs = 0;
+                    break;
+            }
+            int totalWage = emphrs * empWagePrHrs;                //Total Wage Computing
+            Console.WriteLine("Employee total Wage: " + totalWage);
+        }
+    }
+}
