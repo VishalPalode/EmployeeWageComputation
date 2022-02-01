c563242746e3e26c8ae4149fe3bfae8aaa38a0bc
VishalPalode <96374249+VishalPalode@users.noreply.github.com>
2022-02-01 15:30:21 +0530

    Total wage for each company

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWage
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("welcome");
+
+            EmpWage dMart = new EmpWage("DMart", 20, 2, 10);
+            EmpWage reliance = new EmpWage("Reliance", 10, 4, 10);
+            dMart.computeEmpWage();
+            Console.WriteLine(dMart.toString());
+            reliance.computeEmpWage();
+            Console.WriteLine(reliance.toString());
+
+            Console.ReadLine();
+
+        }
+    }
+}
