b1be7e23d31a89eecb01647cf3e0e6511c6e7759
VishalPalode <96374249+VishalPalode@users.noreply.github.com>
2022-02-01 16:23:53 +0530

    Ability to manage EmpWage multi company

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWage
+{
+    internal class Program
+    {
+       static void Main(string[] args)
+        {
+            Console.WriteLine("Hello welcome to EmpWage problem");
+
+            Console.WriteLine("Welcome to the Employee Wage Computation Problem");
+            EmpWageBuilderArray empWageBuilder = new EmpWageBuilderArray();
+            empWageBuilder.addCompanyEmpWage("DMart", 20, 20, 100);
+            empWageBuilder.addCompanyEmpWage("PVP", 30, 24, 160);
+            empWageBuilder.computeEmpWage();
+            Console.ReadLine();
+        }
+    }
+}
